2713d83665ecd2117f101109f06efc9f769fa042
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-06 12:36:32 -0400

    Creating Data
    Add a razor page to create order.
    Create a form inside the razor page.

---
+++ RPDemoApp/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RPDemoApp.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IFoodData _foodData;
+        private readonly IOrderData _orderData;
+
+        public List<SelectListItem> FoodItems { get; set; }
+
+        [BindProperty]  // Whenever you write a post, you can write to Order property
+        public OrderModel Order { get; set; }
+
+        public CreateModel(IFoodData foodData, IOrderData orderData)
+        {
+            _foodData = foodData;
+            _orderData = orderData;
+        }
+        public async Task OnGet()
+        {
+            var food = await _foodData.GetFood();
+
+            FoodItems = new List<SelectListItem>();
+
+            food.ForEach(x =>
+            {
+                FoodItems.Add(new SelectListItem { Value = x.Id.ToString(), Text = x.Title });
+            });
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            // Check if there is no errors in the form
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            // In real world we need to ask for just the food id,
+            // So we need to create a new method with id parameter
+            // and new stored procedure
+            var food = await _foodData.GetFood();
+
+            Order.Total = Order.Quantity * food.Where(x => x.Id == Order.FoodId).First().Price;
+
+            int id = await _orderData.CreateOrder(Order);
+
+            return RedirectToPage("./Create");
+        }
+    }
+}
